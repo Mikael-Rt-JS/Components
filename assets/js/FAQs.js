f6d21822437692ddf988d81b0a9cf16f54c2e6bb
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2024-06-16 17:13:20 +0400

    Create FAQs.js
    Для страницы либо блока FAQs

---
+++ assets/js/FAQs.js
@@ -0,0 +1,65 @@
+class Faqs{
+  static add_detailsDefault(detailsData,id_addDetails){
+    let addDetailsEl=document.querySelector(id_addDetails);
+    addDetailsEl.innerHTML='';
+    let str="";
+    
+    for(let i=0;i<detailsData.length;i++){
+      str+=`<details class="details">
+        <summary>${detailsData[i]['summary']}</summary>
+        <div class="details_in">
+          <p>
+            ${detailsData[i]['answerEN']}
+          </p>
+        </div>
+      </details>`
+    }
+    addDetailsEl.innerHTML=str;
+
+  }
+  
+  static add_detailsUpdate(detailsData,id_addDetails){
+    let addDetailsEl=document.querySelector(id_addDetails);
+    console.log(detailsData,id_addDetails);
+  }
+
+  static add_detailsMyType(detailsData,id_addDetails){
+    let addDetailsEl=document.querySelector(id_addDetails);
+    console.log(detailsData,id_addDetails);
+  }
+}
+
+// Examples / пример 
+let detailsData=[
+  {
+    summary: "When delivery my order?",
+    answerEN: "Hevo Data is a No-code Data Pipeline that offers a fully managed no-code platform to set up data integration from MySQL and 150+ Data Sources (including 50+ Free Data Sources)and will let you directly load data to a Data Warehouse or the destination of your choice. Its fault-tolerant architecture makes sure that your data is secure and consistent.",
+    answerRU: "Hevo Data — это конвейер данных без кода, который предлагает полностью управляемую платформу без кода для настройки интеграции данных из MySQL и более 150 источников данных (включая более 50 бесплатных источников данных) и позволяет напрямую загружать данные в хранилище данных или пункт назначения по вашему выбору. Его отказоустойчивая архитектура гарантирует безопасность и согласованность ваших данных."
+  },
+  {
+    summary: "When delivery my order?",
+    answerEN: "Hevo Data is a No-code Data Pipeline that offers a fully managed no-code platform to set up data integration from MySQL and 150+ Data Sources (including 50+ Free Data Sources)and will let you directly load data to a Data Warehouse or the destination of your choice. Its fault-tolerant architecture makes sure that your data is secure and consistent.",
+    answerRU: "Hevo Data — это конвейер данных без кода, который предлагает полностью управляемую платформу без кода для настройки интеграции данных из MySQL и более 150 источников данных (включая более 50 бесплатных источников данных) и позволяет напрямую загружать данные в хранилище данных или пункт назначения по вашему выбору. Его отказоустойчивая архитектура гарантирует безопасность и согласованность ваших данных."
+  },
+  {
+    summary: "When delivery my order?",
+    answerEN: "Hevo Data is a No-code Data Pipeline that offers a fully managed no-code platform to set up data integration from MySQL and 150+ Data Sources (including 50+ Free Data Sources)and will let you directly load data to a Data Warehouse or the destination of your choice. Its fault-tolerant architecture makes sure that your data is secure and consistent.",
+    answerRU: "Hevo Data — это конвейер данных без кода, который предлагает полностью управляемую платформу без кода для настройки интеграции данных из MySQL и более 150 источников данных (включая более 50 бесплатных источников данных) и позволяет напрямую загружать данные в хранилище данных или пункт назначения по вашему выбору. Его отказоустойчивая архитектура гарантирует безопасность и согласованность ваших данных."
+  },
+  {
+    summary: "When delivery my order?",
+    answerEN: "Hevo Data is a No-code Data Pipeline that offers a fully managed no-code platform to set up data integration from MySQL and 150+ Data Sources (including 50+ Free Data Sources)and will let you directly load data to a Data Warehouse or the destination of your choice. Its fault-tolerant architecture makes sure that your data is secure and consistent.",
+    answerRU: "Hevo Data — это конвейер данных без кода, который предлагает полностью управляемую платформу без кода для настройки интеграции данных из MySQL и более 150 источников данных (включая более 50 бесплатных источников данных) и позволяет напрямую загружать данные в хранилище данных или пункт назначения по вашему выбору. Его отказоустойчивая архитектура гарантирует безопасность и согласованность ваших данных."
+  },
+  {
+    summary: "When delivery my order?",
+    answerEN: "Hevo Data is a No-code Data Pipeline that offers a fully managed no-code platform to set up data integration from MySQL and 150+ Data Sources (including 50+ Free Data Sources)and will let you directly load data to a Data Warehouse or the destination of your choice. Its fault-tolerant architecture makes sure that your data is secure and consistent.",
+    answerRU: "Hevo Data — это конвейер данных без кода, который предлагает полностью управляемую платформу без кода для настройки интеграции данных из MySQL и более 150 источников данных (включая более 50 бесплатных источников данных) и позволяет напрямую загружать данные в хранилище данных или пункт назначения по вашему выбору. Его отказоустойчивая архитектура гарантирует безопасность и согласованность ваших данных."
+  },
+  {
+    summary: "When delivery my order?",
+    answerEN: "Hevo Data is a No-code Data Pipeline that offers a fully managed no-code platform to set up data integration from MySQL and 150+ Data Sources (including 50+ Free Data Sources)and will let you directly load data to a Data Warehouse or the destination of your choice. Its fault-tolerant architecture makes sure that your data is secure and consistent.",
+    answerRU: "Hevo Data — это конвейер данных без кода, который предлагает полностью управляемую платформу без кода для настройки интеграции данных из MySQL и более 150 источников данных (включая более 50 бесплатных источников данных) и позволяет напрямую загружать данные в хранилище данных или пункт назначения по вашему выбору. Его отказоустойчивая архитектура гарантирует безопасность и согласованность ваших данных."
+  }
+]
+Faqs.add_detailsDefault(detailsData,'#add_details');
